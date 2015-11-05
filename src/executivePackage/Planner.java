2345ef6097e649c919c9aafd5de228dd0d36fef1
Brennan Nichyporuk <brennan.nichyporuk@gmail.com>
2015-11-05 11:46:01 -0500

    Update class description for Pilot, Mapper, Node, and Planner classes.

--- src/executivePackage/Planner.java
+++ src/executivePackage/Planner.java
@@ -2,10 +2,15 @@ package executivePackage;
 
 import basicPackage.IObserver;
 
+/**
+ * This class is responsible for coordinating all responsibilities of the robot.
+ * @author brennanNichyporuk
+ *
+ */
 public class Planner extends Thread implements IObserver {
 	
 	/**
-	 * 
+	 * Instantiates several classes.
 	 */
 	public Planner() {
 		
@@ -14,6 +19,10 @@ public class Planner extends Thread implements IObserver {
 	public void run() {
 		
 	}
+	
+	/**
+	 * Called by observed classes to notify Planner of changes.
+	 */
 	public void update(int x){
 		
 	}
--- src/mapPackage/Node.java
+++ src/mapPackage/Node.java
@@ -1,5 +1,11 @@
 package mapPackage;
 
+/**
+ * Node for use by Mapper class. Each Node represents a discrete amount
+ * of space on the map.
+ * @author brennanNichyporuk
+ *
+ */
 public class Node {
 
 }
