9d6341c0d8d2bbe550bbb53f0d32adfb632b1f05
OlivierSM <olivier.morissette@gmail.com>
2015-11-08 19:59:24 -0500

    Tested odometer and navigation to work within 3 centimeters.

--- src/basicPackage/DesignProject.java
+++ src/basicPackage/DesignProject.java
@@ -1,7 +1,34 @@
 package basicPackage;
 
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.Port;
+
 public class DesignProject {
 	public static void main(String[] args) {
 		// Test Comment
+		EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+		EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
+		Odometer odo = new Odometer(leftMotor, rightMotor, 20, true);
+		Navigation navigator = new Navigation(odo);
+		LCDInfo lcd = new LCDInfo(odo);
+		navigator.turnTo(0, true);
+		navigator.travelTo(60, 0);
+		sleep(3000);
+		navigator.travelTo(60, 60);
+		sleep(3000);
+		navigator.travelTo(0,60);
+		sleep(3000);
+		navigator.travelTo(0, 0);
+		navigator.turnTo(0, true);
+		
+	}
+	public static void sleep(int sleepTime){
+		try {
+			Thread.sleep(sleepTime);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/basicPackage/IObserver.java
+++ src/basicPackage/IObserver.java
@@ -1,9 +1,11 @@
 package basicPackage;
 
+import captureFlagPackage.ClassID;
+
 public interface IObserver {
 	/**
 	 * Must implement update following the observer pattern.
 	 * @param x The integer should represent the id of the particular object that is calling update.
 	 */
-	public void update(int x);
+	public void update(ClassID x);
 }
